a02c6112d994423a15792a15d59a58b5f69a6e93
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-09-18 18:06:00 -0400

    Good place to stop

--- routes/tutorial-router.js
+++ routes/tutorial-router.js
@@ -2,14 +2,33 @@ const express = require('express');
 
 const router = express.Router();
 
-
-router.get('/create-a-diy', (req, res, next) => {
-  res.render('diy-views/create.ejs');
-});
-
-router.post('/process-diy', (req, res, next) => {
-  
-});
+// const myUploader = multer(
+//   {
+//     dest: __dirname + '/../public/uploads/'
+//   }
+// );
+//
+// router.get('/create-a-diy', (req, res, next) => {
+//   res.render('diy-views/create.ejs');
+// });
+//
+// router.post(
+//   '/process-diy',
+//   myUploader.single('diyPhoto'),
+//   (req, res, next) => {
+//   const theDIY = new TutorialModel({
+//     name: dsf,
+//   });
+//   theDIY.save((err) => {
+//     if(err){
+//       next(err);
+//       return;
+//     }
+//     res.render('/');
+//   });
+// });
+//
+//
 
 
 
